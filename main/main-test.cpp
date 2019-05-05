ff320386e217b91150b3e4d1134bd4d4e146c621
pippognetow <djpipponardon@gmail.com>
2019-05-05 21:17:13 +0200

    Lots of bug to fix

--- main/main-test.cpp
+++ main/main-test.cpp
@@ -50,143 +50,192 @@ void clrscr(){
   #endif
 }
 
+//------------------------------------------
+//Colors
 void white(HANDLE color){
   SetConsoleTextAttribute(color, 15);
 }
-
 void yellow(HANDLE color){
   SetConsoleTextAttribute(color, 14);
 }
-
 void green(HANDLE color){
   SetConsoleTextAttribute(color, 10);
 }
-
 void red(HANDLE color){
   SetConsoleTextAttribute(color, 12);
 }
+//------------------------------------------
 
-void inizializeStruct(int &errorFlag, HANDLE color){
-  int stationCont = 0;
-  int mainStationPos[nLines], lineCont = 0;
-  string line;
-  ifstream file ("data/train.txt");
-  int pos = 0, staticPos, i = 0, k = 0;
-  char posChar;
-  cout << "[";
-  yellow(color);
-  cout << "WORKER";
+//------------------------------------------
+//Line separator
+void separator(){
+    string underscore;
+    for (int i = 0; i < 51; i++){
+        underscore += "-";
+        cout << "\r" << underscore << flush;
+    }
+    cout << endl;
+}
+//------------------------------------------
+
+void workerprint(HANDLE color){
+  cout << left << setw(2) << "[";
+  green(color);
+  cout << left << setw(7) << "WORKER";
   white(color);
   cout << "] ";
-  cout << "Checking File";
+}
+
+void checkfiles(int &errorFlag, HANDLE color){
+  string tmp;
+  ifstream file ("data/train.txt");
+  //------------------------------------------
+  //Checking File textes
+  for (int i = 0; i < 8; i++){
+    if (tmp.length() == 3){
+      tmp.clear();
+    }
+    workerprint(color);
+    cout << "Starting Checking Files" << endl;
+    if (i == 0){
+      wait(2000);
+    }
+    cout << left << setw(2) << "[";
+    if (i % 2 != 0){
+      yellow(color);
+    } else {
+      white(color);
+    }
+    cout << left << setw(7) << "WORKER";
+    white(color);
+    cout << "] ";
+    cout << "Checking File";
+    tmp += ".";
+    cout << tmp;
+    wait(150);
+    clrscr();
+  }
+  tmp += ".";
+  workerprint(color);
+  cout << "Starting Checking Files" << endl;
+  cout << left << setw(2) << "[";
+  cout << left << setw(7) << "WORKER";
+  white(color);
+  cout << "] ";
+  cout << "Checking File" << tmp << endl;
+  //------------------------------------------
+  if (file.is_open()){
+    workerprint(color);
+    cout << "Checking Done Successfully\n";
+    white(color);
+  } else {
+    cout << left << setw(2) << "[";
+    red(color);
+    cout << left << setw(7) << "ERROR";
+    white(color);
+    cout << "] ";
+    red(color);
+    cout << "Database File Not Found\n";
+    wait(1000);
+    errorFlag = 1;
+    white(color);
+  }
+  //------------------------------------------
+}
+
+void inizializeStruct(int &errorFlag, HANDLE color){
+  string line, tmp;
+  int pos = 0, staticPos, i = 0, currentLine = 0;
+  char posChar;
+
+  cout << left << setw(2) << "[";
+  cout << left << setw(7) << "WORKER";
+  cout << "] Starting Loading";
   for (int i = 0; i < 3; i++){
     cout << ".";
-    usleep(250000);
+    wait(150);
   }
-  wait(500);
-  clrscr();
-  if (file.is_open()){
-    cout << "[";
-    green(color);
-    cout << "WORKER";
-    white(color);
-    cout << "] ";
-    cout << "Checking Done Successfully\n";
-    while (getline (file, line)){
-      //inizializing lines
-      for (int j = 0; j < nStations; j++){
-        pos = line.find('_');
-        station[j + i].line = line.substr(0, pos);
-      }
-      //inizialiazing stations
-      for (int j = 0; j < nStations; j++)
-      {
+  cout << endl;
+  workerprint(color);
+  cout << "Reading File" << endl;
+  ifstream file ("data/train.txt");
+  while (getline (file, line)){
+    //------------------------------------------
+    //Incrementing line counter of file lines
+    currentLine++;
+    //------------------------------------------
+    //Checking if array is smaller than the database file
+    separator();
+    if (currentLine > nLines){
+      red(color);
+      cout << "\nTraceback:" << endl;
+      wait(250);
+      errorFlag = 1; //setting the errorFlag to 1 to get personalized messagge at the end of loading
+      cout << "\tIt seems that the database hold more train lines than the size of the array" << endl;
+      cout << "\tThis means that not all the lines are probably loaded" << endl;
+      cout << "\tCheck the file or correct the array size" << endl;
+      wait(1000);
+      white(color);
+      cout << "\nPress any key to continue..." << endl;
+      cin.get();  //waiting for keyboard inpunt
+      separator();
+      break;
+    }
+    workerprint(color);
+    cout << "Reading Line Nr: " << currentLine << endl;
+    wait(200);
+    workerprint(color);
+    cout << "Loading Line Name" << endl;
+    wait(50);
+    //inizializing lines
+    for (int j = 0; j < nStations; j++){
+      pos = line.find('_');
+      station[j + i].line = line.substr(0, pos);
+    }
+    workerprint(color);
+    cout << "Starting Loading of All Stations" << endl;
+    wait(200);
+    //inizialiazing stations
+    for (int j = 0; j < nStations; j++){
+      posChar = line[pos];
+      staticPos = pos + 1;
+      while (posChar != '*'){
         posChar = line[pos];
-        staticPos = pos + 1;
-        while (posChar != '*')
-        {
-          posChar = line[pos];
-          if (posChar == ',')
-          {
-              station[j + i].name = line.substr(staticPos, pos - staticPos);
-              if (j == 0)
-              {
-                firstStation[stationCont] = station[j + i].name;
-                stationCont++;
-              }
-              if (station[j + i].name == mainStation)
-              {
-                mainStationPos[lineCont] = j + i;
-                lineCont++;
-              }
-              staticPos = pos + 1;
-              j++;
-          }
-          pos++;
+        if (posChar == ','){
+            station[j + i].name = line.substr(staticPos, pos - staticPos);
+            staticPos = pos + 1;
+            j++;
         }
         pos++;
-        posChar = line[pos];
-        //inizializing times
-        for (int j = 0; j < nStations; j++)
-        {
-            staticPos = pos;
-            while (posChar != '*')
-            {
-              posChar = line[pos];
-              if (posChar == ',')
-              {
-                  stringstream(line.substr(staticPos, pos - staticPos)) >> station[j + i].time;
-                  totalTime[k] += station[j + i].time;
-                  if ( j + i < mainStationPos[k])
-                  {
-                    normalTime[k] += station[j + i].time;
-                  }
-                  else
-                  {
-                    reversedTime[k] += station[j + i].time;
-                  }
-                  staticPos = pos + 1;
-                  j++;
-
-              }
-              pos++;
-            }
-        }
       }
-      k++;
-      i += nStations;
+      pos++;
+      posChar = line[pos];
+      workerprint(color);
+      cout << "Stations Loading Ended Successfully" << endl;
+      wait(50);
+      workerprint(color);
+      cout << "Starting Loading of All Intervals Time" << endl;
+      wait(200);
+      //inizializing times
+      for (int j = 0; j < nStations; j++){
+          staticPos = pos;
+          while (posChar != '*'){
+            posChar = line[pos];
+            if (posChar == ','){
+                stringstream(line.substr(staticPos, pos - staticPos)) >> station[j + i].time;
+                staticPos = pos + 1;
+                j++;
+            }
+            pos++;
+          }
+      }
+      workerprint(color);
+      cout << "Intervals Loading Ended Successfully" << endl;
+      wait(50);
     }
-    file.close();
+    i += nStations;
   }
-  else
-  {
-    cout << "[";
-    red(color);
-    cout << "WORKER";
-    white(color);
-    cout << "] ";
-    red(color);
-    cout << "Database File not found\n";
-    wait(1000);
-    white(color);
-    cout << "[";
-    red(color);
-    cout << "WORKER";
-    white(color);
-    cout << "] ";
-    red(color);
-    cout << "Cannot continue\n";
-    wait(1000);
-    white(color);
-    cout << "[";
-    red(color);
-    cout << "WORKER";
-    white(color);
-    cout << "] ";
-    red(color);
-    cout << "Aborting...\n";
-    errorFlag = 1;
-  }
+  file.close();
 }
 
 void coolOutput(HANDLE color){
@@ -208,7 +257,7 @@ void coolOutput(HANDLE color){
 void workerOutput()
 {
     bool isReversed;
-    int time[nLines], tmp , reverseTime[nLines] , tmpReverse;
+    int time[nLines], tmp , reverseTime[nLines] , tmpReverse, flag = 0;
     int k = 0, j = 0;
     int initialTimeH, initialTimeM;
     int initialSeconds;
@@ -216,10 +265,17 @@ void workerOutput()
     int finalSeconds;
     char c;
     int hour, min, sec;
-    cout << "Insert the initial time(HH:MM): ";
-    cin >> initialTimeH >> c >> initialTimeM;
-    cout << "Insert the final time(HH:MM): ";
-    cin >> finalTimeH >> c >> finalTimeM;
+    //while (flag == 0){
+      cout << "Insert the initial time(HH:MM): ";
+      cin >> initialTimeH >> c >> initialTimeM;
+      cout << "Insert the final time(HH:MM): ";
+      cin >> finalTimeH >> c >> finalTimeM;
+    //  if (c == ':' && initialTimeH >= 0 && initialTimeH <= 24 && initialTimeM >= 0 && initialTimeM <= 59){
+      //  if (finalTimeH >= 0 && finalTimeH <= 24 && finalTimeM >= 0 && finalTimeM <= 59){
+      //    flag = 1;
+    //    }
+    //  }
+  //  }
     initialSeconds = initialTimeH * 60 * 60 + initialTimeM * 60;
     finalSeconds = finalTimeH * 60 * 60 + finalTimeM * 60;
 
@@ -295,18 +351,43 @@ void workerOutput()
 int main(){
   inizializeToZero();
   int errorFlag = 0;
-  HANDLE  color;
+  HANDLE  color = GetStdHandle(STD_OUTPUT_HANDLE);
 
-  color = GetStdHandle(STD_OUTPUT_HANDLE);
+  checkfiles(errorFlag, color);
+  separator();
   inizializeStruct(errorFlag, color);
   white(color);
   if (errorFlag == 1){
-    SetConsoleTextAttribute(color, 12);
+    cout << left << setw(2) << "[";
+    yellow(color);
+    cout << left << setw(7) << "WARN";
+    white(color);
+    cout << "] One or more errors occurred" << endl;
+    cout << left << setw(2) << "[";
+    yellow(color);
+    cout << left << setw(7) << "WARN";
+    white(color);
+    cout << "] It's recommended to check the files" << endl;
+    separator();
+    workerprint(color);
+    cout << "Closing File" << endl;
+    workerprint(color);
+    cout << "Anyway... Loading Finished" << endl;
   } else {
-    coolOutput(color);
+    separator();
+    workerprint(color);
+    cout << "Closing File" << endl;
+    workerprint(color);
+    cout << "Loading Finished Successfully" << endl;
+    separator();
+    wait(2000);
+    //coolOutput(color);
   }
   white(color);
- workerOutput();
+  cout << "\nPress any key to continue..." << endl;
+  cin.get();  //waiting for keyboard inpunt
+  clrscr();
+  workerOutput();
 
   return 0;
 }
