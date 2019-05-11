f36e25e24cf8b7668f2b726432aadee661444780
pippognetow <djpipponardon@gmail.com>
2019-05-11 21:38:51 +0200

    NOW working well

--- main/FINAL/v1.0-SERVER/main.cpp
+++ main/FINAL/v1.0-SERVER/main.cpp
@@ -449,12 +449,12 @@ void htmlparser(string time){
   string tab = "      ";
   string structTime, hour, minute;
   stringstream ss;
-  html.seekg(852, ios::beg);
+  html.seekg(963, ios::beg);
   for(int i = 0; i < 5000; i++){
     html << " " << endl;
   }
   selectionSort();
-  html.seekg(852, ios::beg);
+  html.seekg(963, ios::beg);
   for (int i = 0; i < nLines * 4; i++){
     if (htmlReady[i].sec != 0){
       html << "    <div class='data'>" << endl;
